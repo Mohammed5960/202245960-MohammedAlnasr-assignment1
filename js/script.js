bd8e6454cb9ffd2d56631e6a2f5cd2644e67652c
Mohammed alnasr <s202245960@kfupm.edu.sa>
2026-02-14 09:49:30 +0300

    javas updated

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+const toggleBtn = document.getElementById("themeToggle");
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+});
+
+const form = document.getElementById("contactForm");
+const message = document.getElementById("formMessage");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    message.textContent = "Message sent successfully!";
+    message.style.color = "green";
+
+    form.reset();
+});
 
